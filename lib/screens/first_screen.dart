ee80b0d32b136a8f1b8137037f074cdd5d70c9f8
linkonsq <linkon.sq48@gmail.com>
2024-02-20 18:50:17 +0600

    Sample nav bar

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -10,19 +10,61 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  String currentUrl = 'https://appv2.starkregen.de';
+  bool showNavBar = false;
+
   final FlutterSecureStorage storage = const FlutterSecureStorage();
 
   final controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse('https://app.starkregen.de'));
+    ..loadRequest(Uri.parse(currentUrl))
+    ..setNavigationDelegate(NavigationDelegate(
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url == currentUrl) {
+          setState(() {
+            showNavBar = false;
+          });
+        } else {
+          setState(() {
+            showNavBar = true;
+          });
+        }
+        ;
+        return NavigationDecision.navigate;
+      },
+      onPageStarted: (String url) {
+        print('Page started loading: $url');
+        setState(() {
+          currentUrl = url;
+        });
+      },
+    ));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Webview Test'),
-      // ),
-      body: WebViewWidget(controller: controller),
-    );
+        body: WebViewWidget(controller: controller),
+        bottomNavigationBar: showNavBar
+            ? BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.business),
+                    label: 'Business',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.school),
+                    label: 'School',
+                  ),
+                ],
+                selectedItemColor: Colors.amber[800],
+                onTap: (int index) {
+                  print('Tapped index: $index');
+                },
+              )
+            : null);
   }
 }
